51e5f012e3fac2b79f9507e72bd2481f7352f792
dennis ko <dennis0324@naver.com>
2024-04-01 23:14:19 +0900

    feat: popup manager

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,17 +1,16 @@
-function Popup({ children, isOpen, setIsOpen }) {
-  if (isOpen === true)
-    return (
-      <div className="fixed">
-        <div
-          className="fixed top-0 left-0 h-screen w-screen backdrop-blur-sm"
-          onClick={() => setIsOpen(false)}
-        />
-        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          {children}
-        </div>
+import { removePopup } from "./PopupManager";
+function Popup({ children }) {
+  return (
+    <div className="fixed">
+      <div
+        className="fixed top-0 left-0 h-screen w-screen backdrop-blur-sm"
+        onClick={() => removePopup()}
+      />
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        {children}
       </div>
-    );
-  else return <></>;
+    </div>
+  );
 }
 
 export default Popup;
--- src/components/PopupManager.jsx
+++ src/components/PopupManager.jsx
@@ -1,8 +1,38 @@
-import { useContext } from "react";
-import { PopupContext } from "../App";
+import { createElement, useState } from "react";
+import Popup from "./Popup";
+
+let popupItem = {};
+let setPopupItems = () => {};
+export function addPopup(component) {
+  console.log("addPopup", popupItem);
+  const compo = {
+    component: () => <Popup>{component}</Popup>,
+    name: `popup-${popupItem.length + 1}`,
+  };
+  const arr = [...popupItem, compo];
+  console.log(arr);
+  setPopupItems(arr);
+  console.log("addPopup2", popupItem);
+}
+
+export function removePopup() {
+  console.log("removePopup", popupItem);
+  const removedData = [...popupItem];
+  removedData.pop();
+  setPopupItems(removedData);
+}
 function PopupManager() {
-  const { popupData } = useContext(PopupContext);
-  return <div>{}</div>;
+  const [popupData, setPopupData] = useState([]);
+  popupItem = popupData;
+  setPopupItems = setPopupData;
+
+  return (
+    <div className="popup-manager">
+      {popupData.map((item, index) => {
+        return createElement(item.component, { key: index, isOpen: true });
+      })}
+    </div>
+  );
 }
 
 export default PopupManager;
