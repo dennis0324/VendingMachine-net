300fa02e9bbcbf97b148c9402cae06f5b4d0f5e1
dennis ko <dennis0324@naver.com>
2024-04-01 23:22:37 +0900

    feat: SelectCompo left, right color props

--- src/components/SelectCompo.jsx
+++ src/components/SelectCompo.jsx
@@ -3,13 +3,18 @@ function SelectCompo({
   onRight = () => {},
   left = "확인",
   right = "취소",
+  leftColor = "bg-red-400",
+  rightColor = "bg-gray-500",
 }) {
   return (
     <section className="grid grid-cols-2 gap-2 text-nowrap m-2">
-      <button className="bg-red-400 p-3 rounded-lg" onClick={() => onLeft()}>
+      <button
+        className={leftColor + " p-3 rounded-lg"}
+        onClick={() => onLeft()}
+      >
         {left}
       </button>
-      <button className="bg-gray-500 p-3 rounded-lg" onClick={onRight}>
+      <button className={rightColor + " p-3 rounded-lg"} onClick={onRight}>
         {right}
       </button>
     </section>
