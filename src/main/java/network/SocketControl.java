7944deed14f3e96f198eb31965f40f5a42ad4f4a
minsubak <minsu5875@naver.com>
2024-04-13 21:03:23 +0900

    update: 2024-04-13

--- src/main/java/network/SocketControl.java
+++ src/main/java/network/SocketControl.java
@@ -69,10 +69,14 @@ public class SocketControl {
         public void run() {
 
             // MySQL 접속 정보
-            String[] sqlData = new String[3];
+            String[] sqlData = new String[4];
             String sql = null;
 
+            PreparedStatement preparedStatement = null;
+            Connection connection = null;
+
             try {
+                // 클라이언트 메세지 변수
                 InputStream inputStream = clientSocket.getInputStream();
                 final InputStreamReader streamReader = new InputStreamReader(inputStream);
                 BufferedReader br = new BufferedReader(streamReader);
@@ -82,6 +86,7 @@ public class SocketControl {
                     FileReader fileReader = new FileReader("src/main/rsc/mysql.txt");
                     BufferedReader bufferedReader = new BufferedReader(fileReader);
 
+                    // sql 접근 데이터 처리
                     String tmpLine;
                     int i = 0;
                     while((tmpLine = bufferedReader.readLine()) != null) {
@@ -92,15 +97,12 @@ public class SocketControl {
                     e.printStackTrace();
                 }
 
-                Connection connection = null;
-                PreparedStatement preparedStatement = null;
-                ResultSet resultSet = null;
-
                 try {
                     // MySQL 연결
                     connection = DriverManager.getConnection(sqlData[0], sqlData[1], sqlData[2]);
 
                     String line = null;
+                    boolean addExec = false;
                     while ((line = br.readLine()) != null) {
                         // 전송받은 데이터 출력
                         System.out.println(line);
@@ -115,20 +117,34 @@ public class SocketControl {
                         }
 
                         // 전송받은 메세지 분리
-                        String dataSeparation[] = extractStrings(classification[4]);
+                        // String[] dataSeparation = extractStrings(classification[classification.length - 1]);
 
-                        if (debug_mode) { // 디버그 출력
-                            for (String s : dataSeparation) {
-                                System.out.println("[디버그-페이로드 분리 확인]: " + s);
+//                        if (debug_mode) { // 디버그 출력
+//                            for (String s : dataSeparation) {
+//                                System.out.println("[디버그-페이로드 분리 확인]: " + s);
+//                            }
+//                        }
+
+                        try {
+                            if (!addExec) { // 쿼리 추가 준비/실행
+                                preparedStatement = connection.prepareStatement("CALL ADD_MACHINE(?, ?)");
+                                preparedStatement.setString(1, classification[2]);
+                                preparedStatement.setString(2, "null");
+                                preparedStatement.executeQuery();
+                                addExec = true;
                             }
+                        } catch (SQLIntegrityConstraintViolationException e) {
+                            e.printStackTrace();
                         }
+
 
                         // CMD | HASH | ID | DATE | PAYLOAD
                         // 메세지 명령 분류 영역
                         switch (classification[0]) {
                             case "handshake":
                                 System.out.println("[알림]: CMD 코드 - handshake");
-                                receiveMSG = classification[0] + "|" + "|" + String.format("%04d", Counter++)+ "|" + classification[3] + "|" + "\"\"";
+                                Counter++;
+                                receiveMSG = classification[0] + "|" + "|" + String.format("%04d", Counter)+ "|" + classification[3] + "|" + "\"\"";
                                 sendToClient(receiveMSG);
                                 System.out.println("[전송]: " + receiveMSG);
 
@@ -136,96 +152,149 @@ public class SocketControl {
 
                             case "products":
                                 System.out.println("[알림]: CMD 코드 - products");
+
+                                receiveMSG = classification[0] + "|" + classification[1] + "|" + classification[2]+ "|" + classification[3] + "|" +
+                                        "\"data\":\"\"";
+                                sendToClient(receiveMSG);
+                                System.out.println("[전송]: " + receiveMSG);
+
                                 break;
 
                             case "supply":
                                 System.out.println("[알림]: CMD 코드 - supply");
+
+                                receiveMSG = classification[0] + "|" + classification[1] + "|" + classification[2]+ "|" + classification[3] + "|" +
+                                        "\"status\":\"success\",\"data\":\"\"";
+                                sendToClient(receiveMSG);
+                                System.out.println("[전송]: " + receiveMSG);
+
                                 break;
 
                             case "change":
                                 System.out.println("[알림]: CMD 코드 - change");
+
+                                receiveMSG = classification[0] + "|" + classification[1] + "|" + classification[2]+ "|" + classification[3] + "|" +
+                                        "\"status\":\"success\",\"data\":[{\"before:productDto,after:productDto\"}]";
+                                sendToClient(receiveMSG);
+                                System.out.println("[전송]: " + receiveMSG);
+
                                 break;
 
                             case "soldout":
                                 System.out.println("[알림]: CMD 코드 - soldout");
+
+                                receiveMSG = classification[0] + "|" + classification[1] + "|" + classification[2]+ "|" + classification[3] + "|" +
+                                        "\"status\":\"success\",\"data\":\"\"";
+                                sendToClient(receiveMSG);
+                                System.out.println("[전송]: " + receiveMSG);
+
                                 break;
 
                             case "insertMoney":
                                 System.out.println("[알림]: CMD 코드 - insertMoney");
+
+                                receiveMSG = classification[0] + "|" + classification[1] + "|" + classification[2]+ "|" + classification[3] + "|" +
+                                        "\"status\":\"success\",\"data\":\"\"";
+                                sendToClient(receiveMSG);
+                                System.out.println("[전송]: " + receiveMSG);
+
                                 break;
 
                             case "retrieveMoney":
                                 System.out.println("[알림]: CMD 코드 - retrieveMoney");
+
+                                receiveMSG = classification[0] + "|" + classification[1] + "|" + classification[2]+ "|" + classification[3] + "|" +
+                                        "\"status\":\"success\",\"data\":[{price:number,qty:number\"\"}]";
+                                sendToClient(receiveMSG);
+                                System.out.println("[전송]: " + receiveMSG);
+
                                 break;
 
                             case "login":
-                                System.out.println("[알림]: CMD 코드 - login");
+//                                System.out.println("[알림]: CMD 코드 - login");
+//
+//                                // 쿼리 준비
+//                                preparedStatement = connection.prepareStatement("CALL GET_LOGIN_INFO(?)");
+//                                preparedStatement.setString(1, sqlData[3]);
+//
+//                                // 쿼리 실행 및 결과 가져오기
+//                                resultSet = preparedStatement.executeQuery();
+//
+//                                // 결과 처리
+//                                while (resultSet.next()) {
+//                                    // 각 행에서 모든 열의 데이터를 가져와서 출력
+//                                    ResultSetMetaData metaData = resultSet.getMetaData();
+//
+//                                    if (debug_mode) { // 디버그 출력
+//                                        int columnCount = metaData.getColumnCount();
+//                                        for (int i = 1; i <= columnCount; i++) {
+//                                            Object value = resultSet.getObject(i);
+//                                            System.out.print(value + "\t");
+//                                        }
+//                                    }
+//
+//                                    String compareTarget = resultSet.getString(3);
+//
+//                                    if(Objects.equals(dataSeparation[1], compareTarget)) {
+//                                        sendToClient("[알림]: 로그인 성공");
+//                                        receiveMSG = classification[0] + "|" + classification[1] + "|" + classification[2] + "|" + classification[3] +
+//                                                "|{\"status\":\"success\",\"data\":\"\"}";
+//                                        sendToClient(receiveMSG);
+//                                        System.out.println("[전송]: " + receiveMSG);
+//                                    }
+//                                    else {
+//                                        sendToClient("[알림]: 로그인 실패");
+//                                        receiveMSG = classification[0] + "|" + classification[1] + "|" + classification[2] + "|" + classification[3] +
+//                                                "|{\"status\":\"deny\",\"data\":\"\"}";
+//                                        sendToClient(receiveMSG);
+//                                        System.out.println("[전송]: " + receiveMSG);
+//                                    }
+//                                }
+                                cmdProcess(connection, sqlData, classification, "CALL GET_LOGIN_INFO(?)");
 
-                                // 쿼리 준비
-                                sql = "CALL GET_LOGIN_INFO('admin')"; // MySQL 저장 프로시저 호출
-                                preparedStatement = connection.prepareStatement(sql);
-
-                                // 쿼리 실행 및 결과 가져오기
-                                resultSet = preparedStatement.executeQuery();
-
-                                // 결과 처리
-                                while (resultSet.next()) {
-                                    // 각 행에서 모든 열의 데이터를 가져와서 출력
-                                    ResultSetMetaData metaData = resultSet.getMetaData();
-
-                                    if (debug_mode) { // 디버그 출력
-                                        int columnCount = metaData.getColumnCount();
-                                        for (int i = 1; i <= columnCount; i++) {
-                                            Object value = resultSet.getObject(i);
-                                            System.out.print(value + "\t");
-                                        }
-                                    }
-
-                                    String compareTarget = resultSet.getString(3);
-
-                                    if(Objects.equals(dataSeparation[1], compareTarget)) {
-                                        sendToClient("[알림]: 로그인 성공");
-                                        receiveMSG = classification[0] + "|" + classification[1] + "|" + classification[2] + "|" + classification[3] +
-                                                "|{\"status\":\"success\",\"data\":\"\"}";
-                                        sendToClient(receiveMSG);
-                                        System.out.println("[전송]: " + receiveMSG);
-                                    }
-                                    else {
-                                        sendToClient("[알림]: 로그인 실패");
-                                        receiveMSG = classification[0] + "|" + classification[1] + "|" + classification[2] + "|" + classification[3] +
-                                                "|{\"status\":\"deny\",\"data\":\"\"}";
-                                        sendToClient(receiveMSG);
-                                        System.out.println("[전송]: " + receiveMSG);
-                                    }
-
-                                }
                                 break;
 
                             case "collectMoney":
                                 System.out.println("[알림]: CMD 코드 - collectMoney");
+
+                                receiveMSG = classification[0] + "|" + classification[1] + "|" + classification[2]+ "|" + classification[3] + "|" +
+                                        "\"status\":\"success\",\"data\":\"\"";
+                                sendToClient(receiveMSG);
+                                System.out.println("[전송]: " + receiveMSG);
+
                                 break;
 
                             case "purchase":
                                 System.out.println("[알림]: CMD 코드 - purchase");
+
+                                receiveMSG = classification[0] + "|" + classification[1] + "|" + classification[2]+ "|" + classification[3] + "|" +
+                                        "\"status\":\"success\",\"data\":\"\"";
+                                sendToClient(receiveMSG);
+                                System.out.println("[전송]: " + receiveMSG);
+
                                 break;
 
                             case "getMoney":
                                 System.out.println("[알림]: CMD 코드 - getMoney");
                                 break;
 
+                            case "quit":
+                                System.out.println("[알림]: CMD 코드 - quit");
+                                preparedStatement = connection.prepareStatement("REMOVE_MACHINE(?)");
+                                preparedStatement.setString(1, classification[2]);
+                                preparedStatement.executeQuery();
+
                             default:
                                 System.out.println("[에러]: CMD 코드 - Unknown CMD Code");
                                 break;
                         }
-
                     }
-
                 } catch (SQLException e) {
                     e.printStackTrace();
                 } finally {
                     // 리소스 해제
                     try {
-                        if (resultSet != null) resultSet.close();
+                        //if (resultSet != null) resultSet.close();
                         if (preparedStatement != null) preparedStatement.close();
                         if (connection != null) connection.close();
                     } catch (SQLException e) {
@@ -255,6 +324,63 @@ public class SocketControl {
         // 해당 클라이언트에게 메시지 전송하는 메소드
         public void sendToClient(String message) {
             clientWriter.println(message);
+        }
+
+        public void cmdProcess(Connection conn, String[] sqlData, String[] cmdData, String query) throws SQLException {
+            System.out.println("[알림]: CMD 코드 - " + cmdData[0]);
+
+            ResultSet rs = null;
+            PreparedStatement ppst = null;
+
+            // 쿼리 준비
+            ppst = conn.prepareStatement(query);
+            ppst.setString(1, sqlData[3]);
+
+            // 쿼리 실행 및 결과 가져오기
+            rs = ppst.executeQuery();
+
+            // 전송받은 메세지 분리
+            String[] payload = extractStrings(cmdData[cmdData.length - 1]);
+
+            // 결과 처리
+            while (rs.next()) {
+                // 각 행에서 모든 열의 데이터를 가져와서 출력
+                ResultSetMetaData metaData = rs.getMetaData();
+
+                if (debug_mode) { // 디버그 출력
+                    int columnCount = metaData.getColumnCount();
+                    for (int i = 1; i <= columnCount; i++) {
+                        Object value = rs.getObject(i);
+                        System.out.print(value + "\t");
+                    }
+                }
+
+                String target = null;
+                switch(cmdData[0]) {
+
+                    case "login":
+                            target = rs.getString("password");
+                        break;
+
+                    default :
+                        break;
+                }
+
+                if(Objects.equals(payload[1], target)) {
+                    sendToClient("[알림]: " + cmdData[0] + "성공");
+                    receiveMSG = cmdData[0] + "|" + cmdData[1] + "|" + cmdData[2] + "|" + cmdData[3] +
+                            "|{\"status\":\"success\",\"data\":\"\"}";
+                    sendToClient(receiveMSG);
+                    System.out.println("[전송]: " + receiveMSG);
+                }
+                else {
+                    sendToClient("[알림]: " + cmdData[0] + "실패");
+                    receiveMSG = cmdData[0] + "|" + cmdData[1] + "|" + cmdData[2] + "|" + cmdData[3] +
+                            "|{\"status\":\"deny\",\"data\":\"\"}";
+                    sendToClient(receiveMSG);
+                    System.out.println("[전송]: " + receiveMSG);
+                }
+            }
         }
     }
 
