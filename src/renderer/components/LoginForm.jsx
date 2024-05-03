45a4c5968199628a66402ffb69ca51a1d9c7e150
dennisKo <dennis0324@naver.com>
2024-05-04 03:44:41 +0900

    fixed: change login state to false

--- src/renderer/components/LoginForm.jsx
+++ src/renderer/components/LoginForm.jsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import ButtonCompo from "./ButtonCompo";
 function LoginForm({ setId, setPassword, submit }) {
+  function handleKeyDown(e){
+    console.log(e);
+    if(e.key === 'Enter')
+      submit();
+  }
   return (
     <section className="flex flex-col justify-center items-center flex-1">
       <div className="bg-gray-300 p-4 rounded-lg">
@@ -17,7 +22,7 @@ function LoginForm({ setId, setPassword, submit }) {
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <ButtonCompo message="로그인" onClick={submit} />
+        <ButtonCompo message="로그인" onClick={submit} onKeyDown={e =>handleKeyDown(e)}/>
       </div>
     </section>
   );
