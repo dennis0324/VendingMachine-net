7e0400d6d83c2fc4a3c880ac47e3e54ee626fb8d
dennisKo <dennis0324@naver.com>
2024-05-05 03:41:15 +0900

    add: MoneyIndicator

--- src/renderer/pages/Home.jsx
+++ src/renderer/pages/Home.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import ItemSelector from "../components/ItemSelector";
 import CartManager from "../components/CartManager";
 import PopupManager from "../components/PopupManager";
+import MoneyIndicator from "../components/CartManager/MoneyIndicator";
 
 function Home() {
   function goToAdmin() {
@@ -15,6 +16,7 @@ function Home() {
       </nav>
       <div className="flex flex-1 flex-row max-sm:flex-col max-sm:py-0 justify-center items-center">
         <section className="max-sm:h-full max-sm:flex max-sm:flex-col max-sm:justify-between max-sm:mb-0 m-6 w-full grid grid-cols-6 justify-items-center items-center">
+          <MoneyIndicator className={"col-span-6 grid justify-center"}/>
           <ItemSelector className={"col-span-4 mx-5"} />
           <CartManager className={"col-span-2"} />
         </section>
