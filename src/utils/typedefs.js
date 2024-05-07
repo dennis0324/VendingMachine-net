558cd94cd4fe4a466456c44ad829a2c37883ab5f
dennis ko <dennis0324@naver.com>
2024-05-08 02:37:47 +0900

    add: type defination for jsDoc and annotations

---
+++ src/utils/typedefs.js
@@ -0,0 +1,29 @@
+/**
+ * @module types
+ */
+/**
+ * @typedef ipcDto
+ * @type {Object}
+ *
+ * @property {string} hash - the hahs value of cmd
+ * @property {string} cmd - the cmd that operate
+ * @property {string} vendingId - the vending machine id
+ * @property {string} date - the date of the operation
+ * @property {Object} payload - the data to be sent
+ */
+
+/**
+ * @typedef ProductDto
+ * @type {Object}
+ *
+ * @property {string} productId - the name of the product
+ * @property {string} price - the price of the product
+ * @property {int} qty - the quantity of the product
+ * @property {string} name - the name of the product
+ * @property {string} price - the price of the product
+ */
+
+/**
+ */
+
+export {};
