a3d6338c49822fb33fb39c48f4c6205807bd14d7
dennis ko <dennis0324@naver.com>
2024-05-13 01:55:22 +0900

    add: password related constants value

--- src/renderer/utils/constants.js
+++ src/renderer/utils/constants.js
@@ -1,5 +1,10 @@
 export const TEXT = {
-  NOSELECT: "선택하신 데이터가 존재하지 않습니다."
-}
+  NOSELECT: "선택하신 데이터가 존재하지 않습니다.",
+  WRONG_PASSWORD: "",
+  UNMATCH_PASSWORD: "비밀번호가 일치하지 않습니다.",
+  SUCCESS_CHANGEPASSWORD: "비밀번호 변경이 완료되었습니다.",
+  FAIL_CHANGEPASSWORD: "비밀번호 변경에 실패했습니다.",
+};
 
-export const Money = [[10],[50],[100],[500],[1000,5]];
+export const Money = [[10], [50], [100], [500], [1000, 5]];
+
