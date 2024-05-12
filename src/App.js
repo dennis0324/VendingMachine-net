033065771b97d6d9e63717efc8356b845fed4f72
dennis ko <dennis0324@naver.com>
2024-05-13 01:56:58 +0900

    remove: Update Provider

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Admin from "./renderer/pages/Admin";
 import Home from "./renderer/pages/Home";
 import CartProvider from "./renderer/components/CartProvider";
+import MoneyProvider from "./renderer/components/MoneyProvider";
 
 // water 450, coffee 500, energydrink 550, premiumcoffee 700, coke 750,
 // specialdrink 800
@@ -14,12 +15,14 @@ export const VendingMContext = createContext();
 function App() {
   return (
     <CartProvider>
-      <Router basename="/">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </Router>
+      <MoneyProvider>
+        <Router basename="/">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </Router>
+      </MoneyProvider>
     </CartProvider>
   );
 }
