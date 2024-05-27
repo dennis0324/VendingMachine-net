94c52fd11f98263db77901be99d46aabbd026f75
dennis ko <dennis0324@naver.com>
2024-05-27 12:04:41 +0900

    remove: devtools

--- src/main.js
+++ src/main.js
@@ -14,13 +14,14 @@ const createWindow = () => {
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
+    autoHideMenuBar: true,
   });
 
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
